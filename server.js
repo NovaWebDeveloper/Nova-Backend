bebb269b01f55a07c2ac1043bb315223eb84c2ea
Misbah Ehsan <alimza346@gmail.com>
2026-05-23 19:52:31 +0500

    Issue Fixed
    Issue fixed

--- server.js
+++ server.js
@@ -23,10 +23,15 @@ const db = new Pool({
 });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
